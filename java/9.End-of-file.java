b039598df4b5ebd1d27434f0146e666219f54b5a
isilay-subasi <isil.subasi19@gmail.com>
2021-05-23 14:30:14 +0300

    Nine Problem

---
+++ java/9.End-of-file.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan=new Scanner(System.in); 
+        int i=0;
+        while(scan.hasNextLine()){
+            System.out.println(++i+" "+scan.nextLine());
+            
+        }     
+        
+    }
+}
