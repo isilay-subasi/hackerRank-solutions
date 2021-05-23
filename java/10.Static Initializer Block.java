aa0a2812b814245afcb16d36241c4ffe9ab74408
isilay-subasi <isil.subasi19@gmail.com>
2021-05-23 14:32:43 +0300

    Ten Problem

---
+++ java/10.Static Initializer Block.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+static Scanner scan=new Scanner(System.in);
+public static boolean flag=true;
+static int B=scan.nextInt();
+static int H=scan.nextInt();
+static{
+try{
+        if(B <= 0 || H <= 0){
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }catch(Exception e){
+        System.out.println(e);
+    }
+
+    
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
